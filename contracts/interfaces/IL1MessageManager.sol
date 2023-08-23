7e2a2bfb5c03ad60a7b7e66449ec757da6e008b2
thedarkjester <grant.southey@consensys.net>
2023-08-23 11:37:28 +0100

    add message hash to claimed or missing event

--- contracts/interfaces/IL1MessageManager.sol
+++ contracts/interfaces/IL1MessageManager.sol
@@ -20,7 +20,7 @@ interface IL1MessageManager {
   /**
    * @dev Thrown when the message has already been claimed.
    */
-  error MessageDoesNotExistOrHasAlreadyBeenClaimed();
+  error MessageDoesNotExistOrHasAlreadyBeenClaimed(bytes32 messageHash);
 
   /**
    * @dev Thrown when the message has already been received.
